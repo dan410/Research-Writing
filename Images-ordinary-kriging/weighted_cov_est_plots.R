9f77cf489f01ae0cb6fae0fa5ee8920e366d1289
Fortin, Daniel C <daniel.fortin@pnnl.gov>
2015-08-30 12:45:04 -0700

    add simulation results and update weighted covariance estimation plots and writing

--- Images-ordinary-kriging/weighted_cov_est_plots.R
+++ Images-ordinary-kriging/weighted_cov_est_plots.R
@@ -16,7 +16,18 @@ theme_bw()+
 labs(x = "", y = "")
 gp
 
-ggsave("Plots/grid.pdf", height = 4, width = 5)
+# ggsave("Plots/grid.pdf", height = 4, width = 5)
+
+locs$grid <- factor(locs$grid, levels = c('3', '1', '2'), labels = c('low clustering', 'moderate clustering', 'strong clustering'))
+
+ggplot(locs, aes(x = x, y = y)) + 
+  geom_point() + 
+  facet_wrap(~grid) +
+  theme_bw()+
+  theme(strip.text.x = element_text(size = 14))+
+  theme(panel.margin = unit(2,'lines'))+
+  labs(x = "", y = "") 
+# ggsave("Plots/grids.pdf", height = 5, width = 12)
 
 ############################################################################
 ### Plot spatial correlation curves
@@ -31,6 +42,10 @@ cov.f <- function(x,...){
 
 # plot exponential covariance functions
 x <- seq(0,1, length = 1000)
+exp_r4 <- cov.f(x, cov.model="exponential", cov.pars=c(1,.4))
+exp_r4_df <- data.frame(x = x, y = exp_r4, range = 0.4)
+
+x <- seq(0,1, length = 1000)
 exp_r3 <- cov.f(x, cov.model="exponential", cov.pars=c(1,.3))
 exp_r3_df <- data.frame(x = x, y = exp_r3, range = 0.3)
 
@@ -42,21 +57,32 @@ x <- seq(0,1, length = 1000)
 exp_r1 <- cov.f(x, cov.model="exponential", cov.pars=c(1,.1))
 exp_r1_df <- data.frame(x = x, y = exp_r1, range = 0.1)
 
-cov_df <- rbind(exp_r1_df, exp_r2_df, exp_r3_df)
+x <- seq(0.001,1, length = 1000)
+exp_r0 <- cov.f(x, cov.model="exponential", cov.pars=c(1,0))
+exp_r0_df <- data.frame(x = x, y = exp_r0, range = 0.0)
+
+
+cov_df <- rbind(exp_r0_df, exp_r1_df, exp_r2_df, exp_r3_df, exp_r4_df)
 cov_df$range <- factor(cov_df$range)
 
 gp <- ggplot(cov_df, aes(x = x, y = y, group=range, linetype = range, color = range)) + 
 	geom_line()+
 	theme_bw()+
-  scale_color_manual(values = c("cornflowerblue", "darkolivegreen3", "firebrick1"))+
-	labs(x = "", y = "correlation")+
-	guides(color = guide_legend('range'), linetype = guide_legend('range'))
+  scale_color_manual(values = c('purple', "cornflowerblue", "darkolivegreen3", "firebrick1", 'black'))+
+  theme(strip.text.x = element_text(size = 14),
+        axis.text = element_text(size = 10),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 14))+
+	labs(x = "distance", y = "correlation")+
+	guides(color = guide_legend('Range'), linetype = guide_legend('Range'))
 gp
 
 # ggsave("Plots/exp_corr_funs.pdf", width = 8, height = 5)
 
 ############################################################################
-### Plot results of covariance function estimates for different levels of spatial dependence and different spatial weights
+### Plot results of covariance function estimates for different levels of 
+### spatial dependence and different spatial weights (old)
 ############################################################################
 
 ### Read in sim results
@@ -79,6 +105,45 @@ labs(x="weight parameter, p", y = "mean integrated squared error", color = "spat
 gp
 
 # ggsave("Plots/MSE_trends.pdf", width = 8, height = 5)
+
+############################################################################
+### Plot results of covariance function estimates for different levels of 
+### spatial dependence and different spatial weights
+############################################################################
+grid_id <- 1
+grid1 <- readRDS(paste('Data/grid', grid_id, '_res.rds', sep = ''))
+grid1$grid <- grid_id
+grid_id <- 2
+grid2 <- readRDS(paste('Data/grid', grid_id, '_res.rds', sep = ''))
+grid2$grid <- grid_id
+grid_id <- 3
+grid3 <- readRDS(paste('Data/grid', grid_id, '_res.rds', sep = ''))
+grid3$grid <- grid_id
+
+grid <- rbind(grid1, grid2, grid3)
+
+grid$grid <- factor(grid$grid, levels = c('3', '1', '2'), labels = c('low clustering', 'moderate clustering', 'strong clustering'))
+
+grid$range <- factor(grid$range)
+
+L2_stats <- ddply(grid, .(grid, range, weight), summarize, mean = mean(L2), med = median(L2), se = sd(L2)/sqrt(200))
+
+ggplot(L2_stats, aes(x = weight, y = mean, color = range, linetype = range)) +
+  geom_line()+ 
+  geom_errorbar(aes(ymax = mean + se, ymin = mean - se), width = 0.01, linetype = 1)+
+  scale_color_manual(values = c('purple', "cornflowerblue", "darkolivegreen3", "firebrick1", 'black'))+
+  facet_wrap(~grid)+
+  theme_bw()+
+  theme(strip.text.x = element_text(size = 14),
+        axis.text = element_text(size = 10),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 14))+
+  labs(x = 'weight parameter, p', y = "L")+
+  guides(color = guide_legend('Range'), linetype = guide_legend('Range'))
+
+# ggsave(file.path('Plots', 'MSE_trends_all.pdf'), width = 12, height = 5)
+
 
 ############################################################################
 ### 
